4d58ca72108765843f01164017aa2621bd2b8804
somni <me@somni.one>
2021-06-06 23:57:42 +0900

    Add build dependency package installation script

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+
+# Install build dependencies (Ubuntu)
 sudo apt update
 sudo apt install --no-install-recommends dpkg-dev debhelper libgd-dev libgeoip-dev libhiredis-dev libluajit-5.1-dev libmaxminddb-dev libmhash-dev libpam0g-dev libpcre3-dev libperl-dev libssl-dev libxslt1-dev po-debconf quilt

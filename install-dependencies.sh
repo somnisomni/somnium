79c26e086bf8a55f1b16e57fadfcbc57e00f0d97
somni <me@somni.one>
2022-06-13 23:04:35 +0900

    GCC 11

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,4 +2,19 @@
 
 # Install build dependencies (Ubuntu)
 sudo apt update
-sudo apt install --no-install-recommends dpkg-dev debhelper libgd-dev libgeoip-dev libhiredis-dev libluajit-5.1-dev libmaxminddb-dev libmhash-dev libpam0g-dev libpcre3-dev libperl-dev libssl-dev libxslt1-dev po-debconf quilt
+sudo apt install --no-install-recommends \
+  dpkg-dev \
+  debhelper \
+  libgd-dev \
+  libgeoip-dev \
+  libhiredis-dev \
+  libluajit-5.1-dev \
+  libmaxminddb-dev \
+  libmhash-dev \
+  libpam0g-dev \
+  libpcre3-dev \
+  libperl-dev \
+  libssl-dev \
+  libxslt1-dev \
+  po-debconf \
+  quilt

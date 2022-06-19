cef4b54357a5847c582660819af5dc7e81d3f114
somni <me@somni.one>
2022-06-19 21:27:09 +0900

    Reduce build dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,14 +5,13 @@ sudo apt update
 sudo apt install --no-install-recommends \
   dpkg-dev \
   debhelper \
+  libexpat-dev \
   libgd-dev \
   libgeoip-dev \
   libhiredis-dev \
-  libluajit-5.1-dev \
   libmaxminddb-dev \
   libmhash-dev \
   libpam0g-dev \
-  libpcre3-dev \
   libperl-dev \
   libssl-dev \
   libxslt1-dev \
